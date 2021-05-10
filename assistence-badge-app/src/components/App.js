f4be29ce96216bfbf9a4168765ccdc3514a535fc
Karen Celis <k-celis@javeriana.edu.co>
2021-05-10 11:13:27 -0500

    home page

--- assistence-badge-app/src/components/App.js
+++ assistence-badge-app/src/components/App.js
@@ -5,6 +5,7 @@ import Badges from "../pages/Badges";
 import BadgeNew from "../pages/BadgeNew";
 import NotFound from '../pages/NotFound';
 import Layout from "./Layout";
+import Home from "../pages/Home";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/badges" component={Badges}></Route>
           <Route exact path="/badges/new" component={BadgeNew}></Route>
+          <Route exact path="" component={Home}></Route>
           <Route component={NotFound}/>
         </Switch>
       </Layout>
---
+++ assistence-badge-app/src/pages/Home.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles/Home.css';
+import confLog from "../images/el-planeta-tierra.svg";
+import handImage from '../images/girls.svg';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="container">
+          <div className="row">
+            <div className="Home__col col-12 col-md-4">
+              <img
+                src={confLog}
+                alt="Platzi Conf Logo"
+                className="img-fluid mb-2"
+              />
+
+              <h1>Badge Management System</h1>
+              <Link className="btn btn-primary" to="/badges">
+                Start
+              </Link>
+            </div>
+
+            <div className="Home__col d-none d-md-block col-md-8">
+              <img
+                src={handImage}
+                alt="Astronauts"
+                className="img-fluid p-4"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
