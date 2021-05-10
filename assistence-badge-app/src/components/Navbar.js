7f77c79161d99b3aa36db23ba7dad093e9315179
Karen Celis <k-celis@javeriana.edu.co>
2021-05-10 13:07:52 -0500

    life cycle component practice

--- assistence-badge-app/src/components/Navbar.js
+++ assistence-badge-app/src/components/Navbar.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "./styles/Navbar.css";
 import confLog from "../images/el-planeta-tierra.svg";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 class Navbar extends React.Component {
   render() {
     return (
       <div className="Navbar">
         <div className="container-fluid">
           <Link className="Navbar__brand" to="">
-            <img className="Navbar__brand-logo" width="50" height="50"src={confLog} alt="Logo" />
-            <span className="fw-light">Earth</span>
-            <span className="fw-bold">Conf</span>
+            <img
+              className="Navbar__brand-logo"
+              width="50"
+              height="50"
+              src={confLog}
+              alt="Logo"
+            />
+            <span className="fw-light" style={{ color: "white" }}>
+              Earth
+            </span>
+            <span className="fw-bold" style={{ color: "white" }}>
+              Conf
+            </span>
           </Link>
         </div>
       </div>
